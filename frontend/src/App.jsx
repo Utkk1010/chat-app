1c69631fad806404f96caa8808dffc6e4afcb530
Utkk1010 <utkarshutk10@gmail.com>
2024-12-13 21:29:27 +0530

    continuing with frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { Navbar } from './components/Navbar'
+import Navbar  from './components/Navbar'
 import {Route, Routes} from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import SignUpPage from "./pages/SignUpPage"
@@ -8,6 +8,7 @@ import ProfilePage from "./pages/ProfilePage"
 import LoginPage from "./pages/LoginPage"
 import { useAuthStore } from './store/useAuthStore'
 import {Loader} from "lucide-react"
+import {Toaster} from "react-hot-toast"
 
 
 const App = () => {
@@ -20,7 +21,7 @@ const App = () => {
 
   console.log({authUser});
 
-  if(true) return(
+  if(isCheckingAuth && !authUser) return(
     <div className='flex items-center justify-center h-screen'>
       <Loader className="size-10 animate spin"/>
     </div>
@@ -32,16 +33,18 @@ const App = () => {
 
       <Routes>
 
-        <Route path='/' element={<HomePage />}/>
-        <Route path='/signup' element={<SignUpPage />}/>
-        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />}/>
+        <Route path="/signup" element={authUser ? <SignUpPage /> : <Navigate to = "/" />}/>
+        <Route path='/login' element={authUser ? <LoginPage /> : <Navigate to = "/" />}/>
         <Route path='/settings' element={<SettingsPage />}/>
-        <Route path='/profile' element={<ProfilePage />}/>
+        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to ="/login" />}/>
 
       </Routes>
 
+      <Toaster />
+
     </div>
   )
-}
+};
 
 export default App
